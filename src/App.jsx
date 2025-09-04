05e6eacdd1b4d2db288b3b6e000ce2104517782f
katarina-andrews <andrews.katarina@gmail.com>
2025-09-04 15:17:52 -0500

    added header, main component, and footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Adopt Me</h1>
+      </header>
+      <Main />
+      <footer>&copy; Katarina Andrews</footer>
+    </>
+  );
+}
+
+export default App;
